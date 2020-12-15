53d2cadedb7f07d786d4df50fa640b8023d1f10b
HP <lupa.96@live.it>
2020-12-15 17:34:22 +0100

    removed all

--- semantic_maps/src/utils/utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <sys/time.h>
-
-double getTime();
-
-inline double tv2sec(const struct timeval& tv_) {return (static_cast<double>(tv_.tv_sec)+1e-6*static_cast<double>(tv_.tv_usec));}
