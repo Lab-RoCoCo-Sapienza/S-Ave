32b67c806eb0bc6278b9c77e7534f876325927c5
Vincenzo Suriani <vincsur@gmail.com>
2021-05-14 00:04:07 +0200

    plo

--- semantic_maps/results/rosmeery_plot.sh
+++ semantic_maps/results/rosmeery_plot.sh
@@ -16,13 +16,17 @@ mkdir -p ${PLOTS_SAVE_PATH} &> /dev/null
 
 declare -a environments=("apartment_4" "rococo_lab" "phd_office" "prof_office")
 declare -a exploration_algorithms=("frontier_exploration" "teleop" "random_walk")
+declare -a semantic_exploration=("ave" "s-ave")
 
 ## now loop through the above array
 for env in "${environments[@]}" ; do
     echo -ne "-- Plotting metrics from environment ${orange}${RESULTS_LOAD_PATH}${yellow}${env}${nc}\n"
     json_load_paths=""
     for alg in "${exploration_algorithms[@]}" ; do
-        json_load_paths="${json_load_paths} ${RESULTS_LOAD_PATH}/${env}/${alg}/"
+        #json_load_paths="${json_load_paths} ${RESULTS_LOAD_PATH}/${env}/${alg}/"
+        for sem in "${semantic_exploration[@]}" ; do
+            json_load_paths="${json_load_paths} ${RESULTS_LOAD_PATH}/${env}/${alg}/${sem}/"
+        done
     done
 
 # ori_distance
