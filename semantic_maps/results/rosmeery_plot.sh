6cfec56a3ee7b3132f86deb08eab9ea0f5da59d4
Vincenzo Suriani <vincsur@gmail.com>
2021-05-14 11:18:45 +0200

    plot

--- semantic_maps/results/rosmeery_plot.sh
+++ semantic_maps/results/rosmeery_plot.sh
@@ -15,18 +15,18 @@ PLOTS_SAVE_PATH=/tmp/rosmeery/plots/
 mkdir -p ${PLOTS_SAVE_PATH} &> /dev/null
 
 declare -a environments=("apartment_4" "rococo_lab" "phd_office" "prof_office")
-declare -a exploration_algorithms=("frontier_exploration" "teleop" "random_walk")
-declare -a semantic_exploration=("ave" "s-ave")
+declare -a exploration_algorithms=("frontier_exploration" "teleop" "random_walk" "ave" "s-ave")
+#declare -a semantic_exploration=("ave" "s-ave")
 
 ## now loop through the above array
 for env in "${environments[@]}" ; do
     echo -ne "-- Plotting metrics from environment ${orange}${RESULTS_LOAD_PATH}${yellow}${env}${nc}\n"
     json_load_paths=""
     for alg in "${exploration_algorithms[@]}" ; do
-        #json_load_paths="${json_load_paths} ${RESULTS_LOAD_PATH}/${env}/${alg}/"
-        for sem in "${semantic_exploration[@]}" ; do
-            json_load_paths="${json_load_paths} ${RESULTS_LOAD_PATH}/${env}/${alg}/${sem}/"
-        done
+        json_load_paths="${json_load_paths} ${RESULTS_LOAD_PATH}/${env}/${alg}/"
+        #for sem in "${semantic_exploration[@]}" ; do
+        #    json_load_paths="${json_load_paths} ${RESULTS_LOAD_PATH}/${env}/${alg}/${sem}/"
+        #done
     done
 
 # ori_distance
